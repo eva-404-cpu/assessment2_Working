f362cc8d3bfa234d32eaa08b0acc23f83ef912e7
eva-404-cpu <evanicole972@gmail.com>
2026-04-26 21:20:00 +0100

    Changed the location of 'script' to the public folder

--- script.js
+++
@@ -1,8 +0,0 @@
-// Code For The 'Back To Top' Button (Source 1)
-let toTopButton = document.getElementById("back_top_button");
-function goToTop() {
-    // For Safairi
-    document.body.scrollTop = 0
-    // For Chrome, Firefox, IE and Opera
-    document.documentElement.scrollTop = 0
-}
